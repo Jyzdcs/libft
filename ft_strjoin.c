d436b0b4029cd86ec40678b0e3a2c71d9458d848
root <ky.claudan@gmail.com>
2024-10-27 14:54:52 +0000

    a la norme

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -15,8 +15,8 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*tab;
-	int	i;
-	int	k;
+	int		i;
+	int		k;
 
 	tab = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (tab == NULL)
